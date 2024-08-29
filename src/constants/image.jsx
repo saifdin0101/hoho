f6c8d9dd585bf1ed7aecd621ad9d337100d8801d
saifdin0101 <saifdin0808@gmail.com>
2024-08-29 12:47:23 +0100

    params

--- src/constants/image.jsx
+++ src/constants/image.jsx
@@ -1,3 +1,8 @@
 export default {
-  Design: require("../images/54645545445.webp"),
+  house: require("../images/houseoftthedragon.jpg"),
+  odity: require("../images/od.jpg"),
+  profity: require("../images/prof.jpg"),
+  vienna: require("../images/vienna.jpg"),
+  watch: require("../images/watch.jpg"),
+  mm: require("../images/m1.jpg"),
 };
--- src/pages/discrPage/discrPage.jsx
+++ src/pages/discrPage/discrPage.jsx
@@ -12,28 +12,32 @@ export const DiscrPage = () => {
   const arr = Array(finding);
   return (
     <div>
-      {arr.map((item, index) => {
-              <div className="h-[100vh] bg-blacklist bg-center w-full flex justify-center items-center ">
-        <div className="flex justify-center  items-center gap-[20px] shadow h-[70vh] w-[80vw] bg-red-500 rounded-[20px]">
-          <div className="bg-red-200 bg-center bg-cover h-[80%] w-[25%] rounded-[20px]">
-            <img src="" alt="" />
-          </div>
-          <div className="bg-blue-200 h-[80%] w-[70%] rounded-[20px]">
-            <div className="mt-10 ml-10 text-4xl font-bold">{item.Title}</div>
-            <div className=" ml-10 flex gap-10">
-              <div className="font-bold text-xl">Genre :</div>
-              <div className=""> {item.genre}</div>
+      
+        
+          <div className="h-[100vh] bg-gradient-to-r from-[#74d1d9] to-[black] bg-center w-full flex justify-center gap-y-[30px] items-center ">
+          <div className="flex justify-center  items-center gap-[20px] shadow h-[70vh] w-[80vw] bg-red-500 rounded-[20px]">
+            <div className=" bg-center bg-cover h-[80%] w-[25%] rounded-[20px]">
+              <img className="bg-center bg-cover overflow-hidden h-full w-full rounded-[20px]" src={finding.image} alt="" />
             </div>
-            <div className="ml-10 border-double border-black border-[1px] w-20">
-              2h:03Min
+            <div className=" text-white font-bold gradd  h-[80%] flex flex-col gap-y-[10px] w-[70%] rounded-[20px]">
+              <div className="mt-10 ml-10 text-4xl font-bold">{finding.Title}</div>
+              <div className=" ml-10 flex gap-10">
+                <div className="font-bold text-xl">Genre :</div>
+                <div className=""> {finding.genre}</div>
+              </div>
+              <div className="ml-10 border-double border-black p-2 border-[1px] w-20">
+                2h:03Min
+              </div>
+              <div className=" ml-10 text-2xl font-bold">Description :</div>
+              <div className="ml-20  "> {finding.discription}</div>
+              <div className="bg-gradient-to-r from-[#74d1d9] to-[black] ml-[42px] h-[80px] w-[200px] font-bold text-3xl flex items-center justify-center rounded-[30px]">Watch Now</div>
             </div>
-            <div className=" ml-10 text-2xl font-bold">Description :</div>
-            <div className="ml-20 "> {item.discription}</div>
           </div>
         </div>
-      </div>
-
-      })}
+  
+        
+             
+     
 
 
     </div>
